0a257d2c03847abea9d338fe918d10460ffed186
Paul McCrory <paulmccrory@hotmail.co.uk>
2017-01-05 16:31:03 +0000

    mounted get/post/delete routes

--- server/api/category/categoryRoutes.js
+++ server/api/category/categoryRoutes.js
@@ -1,5 +1,6 @@
 var router = require('express').Router();
 var logger = require('../../util/logger');
+var controller = require('./categoryController');
 
 // setup boilerplate route jsut to satisfy a request
 // for building
@@ -8,5 +9,14 @@ router.route('/')
     logger.log('Hey from user!!');
     res.send({ok: true});
   });
+
+router.route('/')
+  .get(controller.get)
+  .post(controller.post);
+
+router.route('/:id')
+  .get(controller.getOne)
+  .put(controller.put)
+  .delete(controller.delete);
 
 module.exports = router;
